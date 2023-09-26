66f69cb0b507660a7773f9014081933cc9e6c227
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2023-09-26 18:15:29 +0200

    set with 2 apps

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Greeting.create(message: 'Hello!')
+Greeting.create(message: 'Hello there!')
 Greeting.create(message: 'Hi there!')
 Greeting.create(message: 'Greetings!')
-Greeting.create(message: 'Hey!')
-Greeting.create(message: 'Howdy!')
+Greeting.create(message: 'Hey, how are you!')
+Greeting.create(message: 'Salut!')
